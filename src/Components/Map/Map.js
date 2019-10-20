f1347b06e8e647151d684e42662f7326c14fcb3f
davidagitlen <davidagitlen@gmail.com>
2019-10-20 15:25:36 -0600

    Attempt to implement route rendering with googleMapApiInternals through google-map-react (commented code in Map component), add .env files for development and production, default svgs to test dropping pins with different images based on type

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -5,23 +5,44 @@ import './Map.scss';
 
 const Map = (props) => {
   const { center, zoom } = props;
-  console.log(center, zoom)
+  // const directionRendererOptions = {
+  //   draggable: true,
+  // };
+  // const polylineOptions = {
+  //   draggable: true,
+  //   editable: true
+  // };
+  // const setMap = (args) => {
+  //   const { map, maps }  = args;
+  //   if (maps && typeof (maps.DirectionRenderer) === "function") {
+  //     // clean previous directions rendered to the map;
+  //     const directionDisplay = new maps.DirectionRenderer();
+  //     directionDisplay.setOptions(directionRendererOptions);
+  //     directionDisplay.setMap(maps);
+  //   }
+  //   const routePolyline = new google.maps.Polyline(polylineOptions);
+  //   routePolyline.setMap(maps);
+  // }
   return(
     <div style={{height: '100vh', width: '100%'}}>
       <GoogleMapReact 
         bootstrapURLKeys={{ key: 'AIzaSyDB8SS8Xy8AGlUmcAOQhqurMugTBv31xns' }}
         defaultCenter={center}
         defaultZoom={zoom}
+        yesIWantToUseGoogleMapApiInternals
+        // onGoogleApiLoaded={setMap}
       >
         <Pin 
           lat={39.751774}
           lng={-104.996809}
           text={'Turing'}
+          type='school'
         />
         <Pin
           lat={39.773563} 
           lng={-105.039513}
           text={'David\'s House'}
+          type='house'
         />
       </GoogleMapReact>
     </div>
