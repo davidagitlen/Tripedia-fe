15937ba6878d0bc6e9ff63f2112abc77e53d8524
Chris Lane <pankokrumbs@gmail.com>
2019-10-20 16:31:12 -0600

    Add functionality to the forms container so the user can collapse and show the container

--- src/Components/FormsContainer/FormsContainer.js
+++ src/Components/FormsContainer/FormsContainer.js
@@ -1,25 +1,65 @@
-import React from 'react';
-import './FormsContainer.scss';
-import StartForm from '../StartForm/StartForm';
-import AttractionsForm from '../AttractionsForm/AttractionsForm';
-import AccomidationsForm from '../AccommodationsForm/AccommodationsForm';
-import FoodForm from '../FoodForm/FoodForm';
-import DrinksForm from '../DrinksForm/DrinksForm';
-import ServicesForm from '../ServicesForm/ServicesForm';
-import SubmitTripForm from '../SubmitTripForm/SubmitTripForm';
+import React, { Component } from "react";
+import "./FormsContainer.scss";
+import StartForm from "../StartForm/StartForm";
+import AttractionsForm from "../AttractionsForm/AttractionsForm";
+import AccomidationsForm from "../AccommodationsForm/AccommodationsForm";
+import FoodForm from "../FoodForm/FoodForm";
+import DrinksForm from "../DrinksForm/DrinksForm";
+import ServicesForm from "../ServicesForm/ServicesForm";
+import SubmitTripForm from "../SubmitTripForm/SubmitTripForm";
 
-const FormsContainer = () => {
-  return (
-    <div className='forms-container__container'>
-      <StartForm />
-      <AttractionsForm />
-      <AccomidationsForm />
-      <FoodForm />
-      <DrinksForm />
-      <ServicesForm />
-      <SubmitTripForm />
-    </div>
-  )
+class FormsContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      collapsed: false
+    };
+  }
+
+  collapseFormContainer = e => {
+    const { collapsed } = this.state;
+    e.preventDefault();
+    this.setState({ collapsed: !collapsed });
+  };
+
+  render() {
+    const { collapsed } = this.state;
+
+    if (collapsed) {
+      return (
+        <div className="collapsed-forms__button">
+          <button
+            onClick={e => {
+              this.collapseFormContainer(e);
+            }}
+          >
+            {">"}
+          </button>
+        </div>
+      );
+    } else {
+      return (
+        <div className="not-collapsed-forms__button">
+          <div className="forms-container__container">
+            <StartForm />
+            <AttractionsForm />
+            <AccomidationsForm />
+            <FoodForm />
+            <DrinksForm />
+            <ServicesForm />
+            <SubmitTripForm />
+          </div>
+          <button
+            onClick={e => {
+              this.collapseFormContainer(e);
+            }}
+          >
+            {"<"}
+          </button>
+        </div>
+      );
+    }
+  }
 }
 
 export default FormsContainer;
--- src/Components/StartForm/StartForm.js
+++ src/Components/StartForm/StartForm.js
@@ -6,9 +6,9 @@ const StartForm = () => {
   return(
     <form className='start-form__container'>
       <h2 className='start-form__header'>Start Your Journey</h2>
-      <input type='text' value='start' placeholder='Start'></input>
+      <input type='text' placeholder='Start'></input>
       <p>to:</p>
-      <input type='text' value='End' placeholder='End'></input>
+      <input type='text' placeholder='End'></input>
       <button>Create Trip</button>
     </form>
   )
