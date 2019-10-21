4540f394bb5efd5260223c15db7997acafe64ddb
davidagitlen <davidagitlen@gmail.com>
2019-10-21 16:40:31 -0600

    Adjust styling of custom pin component to align with street directions

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -22,32 +22,33 @@ import './Map.scss';
 
 const Map = (props) => {
   const { center, zoom } = props;
-  // const directionRendererOptions = {
-  //   draggable: true,
-  // };
   const flightPath = [
     { lat: 39.751774, lng: -104.996809 },
     { lat: 39.773563, lng: -105.039513 }
   ];
-  const renderPolylines = (map, maps) => {
-    console.log('map:', map, 'maps:', maps)
+  // const renderPolylines = (map, maps) => {
+  //   console.log('map:', map, 'maps:', maps)
    
     // let geodesicPolyline = new maps.Polyline({
     //   path: flightPath
     // });
     // geodesicPolyline.setMap(map)
     // directionsPath.setMap(map)
-  }
+  // }
   const displayRoute = (map, maps) => {
-    var start = { lat: 39.751774, lng: -104.996809 };
-    var end = { lat: 39.773563, lng: -105.039513 };
-    var request = {
+    let start = { lat: 39.751774, lng: -104.996809 };
+    let end = { lat: 39.773563, lng: -105.039513 };
+    // let start = '4145 Perry St., Denver, CO';
+    // let end = '1331 17th St., Denver, CO';
+    let request = {
       origin: start,
       destination: end,
       travelMode: 'DRIVING'
     };
     let directionsRenderer = new maps.DirectionsRenderer({
-      path: flightPath
+      path: flightPath,
+      draggable: true,
+      suppressMarkers: true
     });
     let directionsService = new maps.DirectionsService();
     directionsService.route(request, function(result, status) {
@@ -56,6 +57,7 @@ const Map = (props) => {
       }
     })
     directionsRenderer.setMap(map)
+    directionsRenderer.setPanel(map)
   }
   // const polylineOptions = {
   //   path: flightPath,
@@ -83,31 +85,17 @@ const Map = (props) => {
         yesIWantToUseGoogleMapApiInternals
         onGoogleApiLoaded={({map, maps}) => displayRoute(map, maps)}
       >
-        <Pin 
-          lat={39.751774}
-          lng={-104.996809}
-          text={'Turing'}
-          type='school'
-        />
-        {/* <Polyline
-          path={flightPath}
-          options={{
-            strokeColor: '#00ffff',
-            strokeOpacity: 1,
-            strokeWeight: 2,
-            icons: [{
-              icon: "hello",
-              offset: '0',
-              repeat: '10px'
-            }],
-          }}
-
-        /> */}
         <Pin
           lat={39.773563} 
           lng={-105.039513}
           text={'David\'s House'}
           type='house'
+        />
+        <Pin 
+          lat={39.751774}
+          lng={-104.996809}
+          text={'Turing'}
+          type='school'
         />
       </GoogleMapReact>
     </div>
