bb7765d6d4414461aa235aab8d9b45b0994be694
davidagitlen <davidagitlen@gmail.com>
2019-10-22 15:37:21 -0600

    Update Navigation.js

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -4,6 +4,9 @@ import Logo from '../../images/Logo1.png';
 
 const Navigation = () => {
   return(
+
+
+    
     <nav className='navigation__contianer'>
       <div className='logo-title__nav'>
         <img className='tripedia-logo__nav' src={Logo} alt='Tripedia Logo'/>
