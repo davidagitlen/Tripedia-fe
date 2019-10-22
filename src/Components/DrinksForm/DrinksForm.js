d2d556edac227a78ff03b0515f1942111c031dcf
David Gitlen <davidagitlen@gmail.com>
2019-10-22 15:06:16 -0600

    Revert "Hooks form"

--- src/Components/DrinksForm/DrinksForm.js
+++ src/Components/DrinksForm/DrinksForm.js
@@ -3,9 +3,7 @@ import './DrinksForm.scss';
 
 const DrinksForm = () => {
   return(
-    <div className='drink-form__container'>
-      <p>DrinksForm</p>
-    </div>
+    <p>DrinksForm</p>
   ) 
 }
 
--- src/Components/FormsContainer/FormsContainer.js
+++ src/Components/FormsContainer/FormsContainer.js
@@ -1,75 +1,10 @@
-import React, { useState } from "react";
-import "./FormsContainer.scss";
-import StartForm from "../StartForm/StartForm";
-import AttractionsForm from "../AttractionsForm/AttractionsForm";
-import AccommidationsForm from "../AccommodationsForm/AccommodationsForm";
-import FoodForm from "../FoodForm/FoodForm";
-import DrinksForm from "../DrinksForm/DrinksForm";
-import ServicesForm from "../ServicesForm/ServicesForm";
-import SubmitTripForm from "../SubmitTripForm/SubmitTripForm";
+import React from 'react';
+import './FormsContainer.scss';
 
-export const FormsContainer = () => {
-  const [collapsed, collapseFormContainer] = useState(false);
-
-  const [openForm, collapseForm] = useState({
-    StartForm: true,
-    AttractionsForm: false,
-    AccommidationsForm: false,
-    FoodForm: false,
-    DrinksForm: false,
-    ServicesForm: false,
-    SubmitTripForm: false
-  })
-
-  const defaultOpenForm = {
-    StartForm: false,
-    AttractionsForm: false,
-    AccommidationsForm: false,
-    FoodForm: false,
-    DrinksForm: false,
-    ServicesForm: false,
-    SubmitTripForm: false
-  };
-
-
-  if (collapsed) {
-    return (
-      <div className="collapsed-forms__button">
-        <button
-          className="collapse_button"
-          onClick={() => {
-            collapseFormContainer(!collapsed);
-          }}
-        >
-          {"||"}
-        </button>
-      </div>
-    );
-  } else {
-    return (
-      <div className="not-collapsed-forms__button">
-        <div className="forms-container__container">
-          <StartForm collapseForm={collapseForm}
-                      openForm={openForm}
-                      defaultForm={defaultOpenForm}/>
-          <AttractionsForm collapseForm={collapseForm}/>
-          <AccommidationsForm collapseForm={collapseForm}/>
-          <FoodForm collapseForm={collapseForm}/>
-          <DrinksForm collapseForm={collapseForm}/>
-          <ServicesForm collapseForm={collapseForm}/>
-          <SubmitTripForm collapseForm={collapseForm}/>
-        </div>
-        <button
-          className="collapse_button"
-          onClick={e => {
-            collapseFormContainer(!collapsed);
-          }}
-        >
-          {"||"}
-        </button>
-      </div>
-    );
-  }
-};
+const FormsContainer = () => {
+  return (
+    <p>FormsContainer</p>
+  )
+}
 
 export default FormsContainer;
