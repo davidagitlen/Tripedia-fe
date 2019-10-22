cbd6d87fbd95b1d0cf71e559e50040c561d9259f
Brandy Mello <brandyamello@gmail.com>
2019-10-22 17:43:03 -0600

    Add email and password inputs with a hook to handle each and store input values a button is conditionally rendered when the inputs are not empty

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginForm.scss';
 
 const LoginForm = () => {
-  return(
-    <p>LoginForm</p>
+  const [emailInput, handleEmail] = useState('');
+  const [passwordInput, handlePassword] = useState('')
+
+  return (
+    <>
+      <p>LoginForm</p>
+      <input
+        type="email"
+        className="email_input"
+        alt="email_input"
+        placeholder="email"
+        value={emailInput}
+        onChange={(e) => handleEmail(e.target.value)}>
+      </input>
+      <input
+        type="password"
+        alt="password_input"
+        className="password_input"
+        placeholder="password"
+        value={passwordInput}
+        onChange={(e) => handlePassword(e.target.value)}>
+      </input>
+      {emailInput && passwordInput && <button
+        className="login_button"
+        type="submit">Login</button>}
+    </>
   )
 }
 
