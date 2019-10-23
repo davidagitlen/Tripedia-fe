eda02c56b2d28c6b422e0f19559c1dcad80b9c60
davidagitlen <davidagitlen@gmail.com>
2019-10-22 22:03:15 -0600

    Add checkboxes with dummy props to FoodForm

--- src/Components/FoodForm/FoodForm.js
+++ src/Components/FoodForm/FoodForm.js
@@ -1,14 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FoodForm.scss';
 
 const FoodForm = ({ collapseForm, openForm, defaultForm }) => {
+  const [form, toggleClicked] = useState({
+    Mexican: false,
+    Thai: false,
+    Chinese: false,
+    Japanese: false,
+    Vietnamese: false,
+    FastFood: false,
+    FineDining: false,
+    Pizza: false
+  });
+
+  let mockProps = [
+    'Mexican',
+    'Thai',
+    'Chinese',
+    'Japanese',
+    'Vietnamese',
+    'Fast Food',
+    'Fine Dining',
+    'Pizza'
+  ];
+
+  const checkBoxes = mockProps.map(checkBox => {
+    let name = checkBox.replace(/ /gi, '');
+    return (
+      <div
+        className='individual-checkbox__container'
+        >
+        <input
+          className='food__checkbox'
+          type='checkbox'
+          value={name}
+          data={form[name]}
+          onClick={e => 
+            toggleClicked({
+              ...form, [e.target.value]: !form[e.target.value]
+            })
+          }
+        />
+        <label>{checkBox}</label>
+      </div>
+    );
+  });
 
   if(openForm.FoodForm) {
     return (
       <div className='food-form__container'>
-        <p>FoodForm</p>
+        <h2>- Food -</h2>
+        <div className='checkbox__container'>
+          {checkBoxes}
+        </div>
       </div>
-    )
+    );
   } else {
     return (
       <div
@@ -19,7 +65,7 @@ const FoodForm = ({ collapseForm, openForm, defaultForm }) => {
             !openForm.FoodForm
           })
         }
-        className="food-form-closed__container"
+        className='food-form-closed__container'
       >
         <p>FoodForm</p>
       </div>
