5883ddabb6cbaad381e2223b032b89c315196b36
davidagitlen <davidagitlen@gmail.com>
2019-10-22 22:15:11 -0600

    Insert checkboxes into AccommodationsForm with dummy props  and add styling

--- src/Components/AccommodationsForm/AccommodationsForm.js
+++ src/Components/AccommodationsForm/AccommodationsForm.js
@@ -1,14 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AccommodationsForm.scss';
 
 const AccommodationsForm = ({ collapseForm, openForm, defaultForm}) => {
 
+  const [form, toggleClicked] = useState({
+    Hotels: false,
+    Motels: false,
+    BedAndBreakfasts: false,
+    Resorts: false,
+    Campgrounds: false,
+    Hostels: false,
+    SkiResorts: false
+  });
+
+  let mockProps = [
+    "Hotels",
+    "Motels",
+    "Bed and Breakfasts",
+    "Resorts",
+    "Campgrounds",
+    "Hostels",
+    "Ski Resorts"
+  ];
+
+  const checkBoxes = mockProps.map(checkBox => {
+    let name = checkBox.replace(/ /gi, "");
+    return (
+      <div className="individual-checkbox__container">
+        <input
+          className="accommodation__checkbox"
+          type="checkbox"
+          value={name}
+          data={form[name]}
+          onClick={e =>
+            toggleClicked({ ...form, [e.target.value]: !form[e.target.value] })
+          }
+        />
+        <label>{checkBox}</label>
+      </div>
+    );
+  });
+
   if(openForm.AccommodationsForm) {
     return (
       <div className='accommodations-form__container'>
-        <p>AccommodationsForm</p>
+        <h2>- Accommodations -</h2>
+        <div className='checkbox__container'>
+          {checkBoxes}
+        </div>
       </div>
-    )
+    );
   } else {
     return (
       <div
