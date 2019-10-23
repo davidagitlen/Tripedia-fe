553eaa9cfe13d2964f650bd0403abf19a0ff1888
davidagitlen <davidagitlen@gmail.com>
2019-10-23 16:39:38 -0600

    Update StartForm to use hook for updating and storing state

--- src/Components/StartForm/StartForm.js
+++ src/Components/StartForm/StartForm.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './StartForm.scss';
 
 const StartForm = ({ collapseForm, openForm, defaultForm}) => {
   
+  const [cities, enterCities] = useState({
+    origin: '',
+    destination: ''
+  });
+
+  const updateCities = e => {
+    enterCities({...cities, [e.target.name]:e.target.value})
+  }
 
   if(openForm.StartForm) {
     return(
       <form className='start-form__container'>
         <h2 className='start-form__header'>Start Your Journey</h2>
-        <input type='text' placeholder='Start'></input>
+        <input 
+          type='text' 
+          name='origin'
+          value={cities.origin}
+          placeholder='Start'
+          onChange={ e => updateCities(e)}
+        />
         <p>to:</p>
-        <input type='text' placeholder='End'></input>
+        <input 
+          type='text' 
+          name='destination'
+          value={cities.destination}
+          placeholder='End'
+          onChange={ e => updateCities(e)}
+        />
         <button>Create Trip</button>
       </form>
     )
