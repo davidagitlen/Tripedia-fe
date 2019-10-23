241a1ce6dde078d5050b08b3b28b4c9cf2f17568
Brandy Mello <brandyamello@gmail.com>
2019-10-22 21:32:38 -0600

    Style disable login button and create functionality

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -1,33 +1,39 @@
 import React, { useState } from 'react';
 import './LoginForm.scss';
+import banner from '../../images/banner.jpg'
 
 const LoginForm = () => {
   const [emailInput, handleEmail] = useState('');
-  const [passwordInput, handlePassword] = useState('')
+  const [passwordInput, handlePassword] = useState('');
+  const isEnabled = emailInput.length > 0 && passwordInput.length > 0;
 
   return (
-    <>
-      <p>LoginForm</p>
-      <input
-        type="email"
-        className="email_input"
-        alt="email_input"
-        placeholder="email"
-        value={emailInput}
-        onChange={(e) => handleEmail(e.target.value)}>
-      </input>
-      <input
-        type="password"
-        alt="password_input"
-        className="password_input"
-        placeholder="password"
-        value={passwordInput}
-        onChange={(e) => handlePassword(e.target.value)}>
-      </input>
-      {emailInput && passwordInput && <button
-        className="login_button"
-        type="submit">Login</button>}
-    </>
+    <main>
+      <img src={banner} alt="" /> 
+      <form className="login_form">
+        <h2>LoginForm</h2>
+        <input
+          type="email"
+          className="email_input"
+          alt="email_input"
+          placeholder="email"
+          value={emailInput}
+          onChange={(e) => handleEmail(e.target.value)}>
+        </input>
+        <input
+          type="password"
+          alt="password_input"
+          className="password_input"
+          placeholder="password"
+          value={passwordInput}
+          onChange={(e) => handlePassword(e.target.value)}>
+        </input>
+        <button
+          disabled={!isEnabled}
+          className="login_button"
+          type="submit">Login</button>
+      </form> 
+    </main>
   )
 }
 
