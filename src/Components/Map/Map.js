35e8fb27e4ca12e067f2a75a773d7d85ef790283
davidagitlen <davidagitlen@gmail.com>
2019-10-24 13:11:50 -0600

    Update Map.js to useState hook instead of class component state

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,44 +1,21 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import GoogleMapReact from 'google-map-react';
-// import Polyline from 'google-map-react';
 import Pin from '../Pin/Pin';
 import './Map.scss';
 
-// const Polyline = () => {
-//   <Polyline
-//     path={pathCoordinates}
-//     options={{
-//       strokeColor: '#00ffff',
-//       strokeOpacity: 1,
-//       strokeWeight: 2,
-//       icons: [{
-//         icon: "hello",
-//         offset: '0',
-//         repeat: '10px'
-//       }],
-//     }}
-//   />
-// }
+const Map = (props) => {
+  const [waypoints, updateWaypoints] = useState([]);
+  const { center, zoom } = props;
+  let start = { lat: 39.739131, lng: -104.990085 };
+  let end = { lat: 40.027750, lng: -105.270350 };
 
-class Map extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      counter: 0,
-      waypoints: []
-    }
-  }
-
-  displayRoute = (map, maps) => {
-    let start = { lat: 39.739131, lng: -104.990085 };
-    let end = { lat: 40.027750, lng: -105.270350 };
-
+  const displayRoute = (map, maps) => {
     console.log(map)
     console.log(maps)
     let request = {
       origin: start,
       destination: end,
-      waypoints: this.state.waypoints,
+      waypoints,
       travelMode: 'DRIVING'
     };
     let directionsRenderer = new maps.DirectionsRenderer({
@@ -51,47 +28,32 @@ class Map extends Component{
       console.log('result', result)
       if (status === 'OK') {
         // getPoints(result);
-        console.log(await result.routes[0].overview_path[0].lat.Scopes)
         directionsRenderer.setDirections(result)
       }
     })
     directionsRenderer.setMap(map)
   }
 
-  
-  render() {
-    console.log('rendering')
-    const { center, zoom } = this.props;
-    let routePoints = [];
-    // const getPoints = async (response) => {
-    //     routePoints = await response.routes[0].overview_path.map(point => ({
-    //     lat: point.lat.scopes.b,
-    //     lng: point.lat.scopes.a
-    //   }))
-    // }
-    // let waypoints = []
-    // if (this.state.count > 0) {
-    //   waypoints = [{ location: { lat: 40, lng: 40 }, stopover: true }]
-    // }
-  
-    return(
-      <div 
-        style={{height: '80vh', width: '100%'}}
+
+  return (
+    <div
+        style={{ height: '80vh', width: '100%' }}
       >
-          <button
-            style={{position: 'absolute', zIndex: 100}}
-          onClick={() => this.setState({ waypoints: [{ location: { lat: 39.734551, lng: -104.832522 }, stopover: true }, { location: { lat: 42.323459, lng: -112.485763 }, stopover: true }, { location: { lat: 35.734551, lng: -100.832522 }, stopover: true } ]})}>
-          </button>
+        <button
+          style={{ position: 'absolute', zIndex: 100 }}
+           onClick={() => updateWaypoints([{ location: { lat: 39.734551, lng: -104.832522 }, stopover: true }, { location: { lat: 39.739131, lng: -104.990085 }, stopover: true }, { location: { lat: 40.734551, lng: -104.832522 }, stopover: true }])
+            }>
+        </button>
         <GoogleMapReact
-          key={this.state.waypoints}
+          key={waypoints}
           bootstrapURLKeys={{ key: 'AIzaSyDB8SS8Xy8AGlUmcAOQhqurMugTBv31xns' }}
           defaultCenter={center}
           defaultZoom={zoom}
           yesIWantToUseGoogleMapApiInternals
-          onGoogleApiLoaded={({map, maps}) => this.displayRoute(map, maps)}
+          onGoogleApiLoaded={({ map, maps }) => displayRoute(map, maps)}
         >
           <Pin
-            lat={39.773563} 
+            lat={39.773563}
             lng={-105.039513}
             text={'David\'s House'}
             type='house'
@@ -106,6 +68,7 @@ class Map extends Component{
       </div>
     )
   }
-}
+
+ 
 
 export default Map;
