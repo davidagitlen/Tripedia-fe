8b03a705ef6c5ef432bc573f692b9b7d49e24f4c
Chris Lane <pankokrumbs@gmail.com>
2019-10-23 18:59:19 -0600

    small refactors

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -1,51 +1,54 @@
-import React, { useState } from 'react';
-import './LoginForm.scss';
-import banner from '../../images/banner.jpg'
-import { NavLink } from 'react-router-dom';
+import React, { useState } from "react";
+import "./LoginForm.scss";
+import { NavLink } from "react-router-dom";
+import banner from "../../images/banner.jpg";
 
 const LoginForm = () => {
-
   const [loginState, handleForm] = useState({
-    email: '',
-    password: ''
-  })
-  const {email, password} = loginState
+    email: "",
+    password: ""
+  });
+  const { email, password } = loginState;
   const isEnabled = email && password;
 
-  const handleChange = (e) => {
-    handleForm({ ...loginState, [e.target.name]: e.target.value })
-  }
+  const handleChange = e => {
+    handleForm({ ...loginState, [e.target.name]: e.target.value });
+  };
 
   return (
-    <main>
-      <img src={banner} alt="Tripedia for all your travel planning needs" /> 
+    <main className='blah'>
+      <img
+        className="banner"
+        src={banner}
+        alt="Tripedia for all your travel planning needs"
+      />
       <form className="login_form">
         <h2>Login Form</h2>
         <input
           className="email_input"
           type="email"
-          name='email'
+          name="email"
           placeholder="email"
           value={email}
-          onChange={handleChange}>
-        </input>
+          onChange={handleChange}
+        ></input>
         <input
           type="password"
-          name='password'
+          name="password"
           className="password_input"
           placeholder="password"
           value={password}
-          onChange={handleChange}>
-        </input>
-        <button
-          disabled={!isEnabled}
-          className="login_button"
-          type="submit">Login
+          onChange={handleChange}
+        ></input>
+        <button disabled={!isEnabled} className="login_button" type="submit">
+          Login
         </button>
-        <NavLink to='/create_account'><p className="create_account_link">Create Account</p></NavLink>
-      </form> 
+        <NavLink to="/create_account">
+          <p className="create_account_link">Create Account</p>
+        </NavLink>
+      </form>
     </main>
-  )
-}
+  );
+};
 
 export default LoginForm;
