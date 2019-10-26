789bbb26e31bec8d3fb2d727009fe3ae611c68f5
davidagitlen <davidagitlen@gmail.com>
2019-10-26 12:50:14 -0600

    Troubleshoot deploying to heroku, rename images file in src directory to Images to match expected file paths.

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './Navigation.scss';
-// import Logo from '../../images/Logo1.png';
+import Logo from '../../Images/Logo1.png';
 import { NavLink } from 'react-router-dom';
+
 
 const Navigation = () => {
   return(
     <nav className='navigation__contianer'>
       <div className='logo-title__nav'>
         <NavLink to='/'>
-          <img className='tripedia-logo__nav' src='' alt='Tripedia Logo' />
+          <img className='tripedia-logo__nav' src={Logo} alt='Tripedia Logo' />
         </NavLink>
         <h1 className='tripedia-text__nav'>TRIPEDIA</h1>
       </div>
