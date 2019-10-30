3510fb6f43605866ff8d3b4641ce9908e41627b6
Chris Lane <pankokrumbs@gmail.com>
2019-10-30 15:46:10 -0600

    Convert forms to use context, add delete commented out code

--- src/Components/AccommodationsForm/AccommodationsForm.js
+++ src/Components/AccommodationsForm/AccommodationsForm.js
@@ -2,41 +2,43 @@ import React, { useState, useContext } from "react";
 import "./AccommodationsForm.scss";
 import { FormContext } from "../../Contexts/FormContext";
 import accommodationSvg from "../../Images/accommodation-pin.svg";
+import { createStateObject, createCheckBoxNames } from "../../util/dataCleaner";
 
-const AccommodationsForm = ({ collapseForm, openForm, defaultForm, formObject }) => {
+const AccommodationsForm = ({
+  collapseForm,
+  openForm,
+  defaultForm,
+  formObject
+}) => {
   const { formState, setFormState } = useContext(FormContext);
-
-  const formCategories = Object.keys(formObject);
-
-  const stateObject = formCategories.reduce((categoryObject, category) => {
-    const categoryKey = category.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join('');
-    categoryObject[categoryKey] = false;
-    return categoryObject
-  }, {});
+  const stateObject = createStateObject(formObject);
+  const checkboxNames = createCheckBoxNames(formObject);
 
   const [form, toggleClicked] = useState({ ...stateObject });
 
-  const checkboxNames = formCategories.map(category => category.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' '));
-
-  const handleCheckBox = (e) => {
+  const handleCheckBox = e => {
     toggleClicked({ ...form, [e.target.value]: !form[e.target.value] });
     const formattedName = e.target.name.toLowerCase();
     if (e.target.checked) {
       const checkedArray = formState.accommodations[formattedName];
-      setFormState({ ...formState, selectedCategories: formState.selectedCategories.concat(checkedArray) })
+      setFormState({
+        ...formState,
+        selectedCategories: formState.selectedCategories.concat(checkedArray)
+      });
     } else {
-      const filteredState = formState.selectedCategories.length ? formState.selectedCategories.filter(object => object.category !== formattedName) : [];
-      setFormState({ ...formState, selectedCategories: filteredState })
+      const filteredState = formState.selectedCategories.length
+        ? formState.selectedCategories.filter(
+            object => object.category !== formattedName
+          )
+        : [];
+      setFormState({ ...formState, selectedCategories: filteredState });
     }
   };
 
   const checkBoxes = checkboxNames.map(checkBox => {
     let name = checkBox.replace(/ /gi, "");
     return (
-      <div
-        key={name}
-        className="individual-checkbox__container"
-      >
+      <div key={name} className="individual-checkbox__container">
         <input
           className="checkbox"
           type="checkbox"
