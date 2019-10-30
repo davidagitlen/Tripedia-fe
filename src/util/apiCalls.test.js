4e49eba423db42e81d331443a6ef02deb59b6aa6
Paul Schlattmann <3031146+pschlatt@users.noreply.github.com>
2019-10-30 17:51:55 -0600

    Adds api key Tests Co-Authored-By: Chris Lane <c.lane.dev@gmail.com>

--- src/util/apiCalls.test.js
+++ src/util/apiCalls.test.js
@@ -1,8 +1,20 @@
+import {createAccount, loginUser, getStartAndEnd, getApiKey} from '../util/apiCalls.js';
 
-describe('apiCalls', () => {
-
-  it('should pass a dummy test', () => {
-    expect(true).toEqual(true);
-  });
-  
-});
+describe('get API key', ()=> {
+  // let mockResponse, mockData;
+  beforeEach(()=>{
+    //  mockData = {name: "paul", email: "paul@gmail.com", password: "123",
+    // password_confirmation: "123"};
+    //  mockResponse = {id: 1, name: "paul", email: "paul@gmail.com", password: "123", account_created: true};
+     window.fetch = jest.fn().mockImplementation(() => {
+       return Promise.resolve({
+         ok: true,
+         json: () => Promise.resolve("string")
+       })
+     })
+  })
+  it.only("Should call fetch with the correct URL", async () => {
+    const key = await getApiKey();
+    expect(key).toEqual("string")
+  })
+})
