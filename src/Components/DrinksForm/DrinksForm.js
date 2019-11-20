716d6564b42ad7d517cf768c8e7eed5c368ede87
davidagitlen <davidagitlen@gmail.com>
2019-11-20 14:28:03 -0700

    Add in useEffect hook to eliminate uncontrolled to controlled component warning

--- src/Components/DrinksForm/DrinksForm.js
+++ src/Components/DrinksForm/DrinksForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import "./DrinksForm.scss";
 import { FormContext } from "../../Contexts/FormContext";
 import drinkSvg from "../../Images/drink-pin.svg";
@@ -9,6 +9,9 @@ const DrinksForm = ({ collapseForm, openForm, defaultForm, formObject }) => {
   const stateObject = createStateObject(formObject);
   const checkboxNames = createCheckBoxNames(formObject);
   const [form, toggleClicked] = useState({ ...stateObject });
+  useEffect(() => {
+    toggleClicked({ ...stateObject });
+  }, [Object.keys(formObject).length > 0])
 
   const handleCheckBox = e => {
     toggleClicked({ ...form, [e.target.value]: !form[e.target.value] });
