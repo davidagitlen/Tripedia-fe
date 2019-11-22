d866756bf9e6137238ac453f8b2b036fed67a049
davidagitlen <davidagitlen@gmail.com>
2019-11-22 14:54:27 -0700

    Remove commented out code

--- src/Components/FormsContainer/FormsContainer.js
+++ src/Components/FormsContainer/FormsContainer.js
@@ -30,84 +30,69 @@ export const FormsContainer = () => {
     ServicesForm: false,
     SubmitTripForm: false
   };
-  const { accommodations, attractions, food, drinks, services} = formState; 
+  const { accommodations, attractions, food, drinks, services } = formState; 
+  const forms = [ accommodations, attractions, food, drinks, services ];
   const collapsedForm = collapsed ? 
   'collapsed-forms__button' :
   'not-collapsed-forms__button';
   const collapsedClass = collapsed ? 'collapsed-forms__container' : 'forms-container__container';
 
-  // if (collapsed) {
-  //   return (
-  //     <div className="collapsed-forms__button">
-  //       <button
-  //         className="collapse_button"
-  //         onClick={() => {
-  //           collapseFormContainer(!collapsed);
-  //         }}
-  //       >
-  //         {"||"}
-  //       </button>
-  //     </div>
-  //   );
-  // } else {
-    return (
-      <div className="wrapper-div">
-        <div className={collapsedForm}>
-          {/* <div className='forms-container__container'> */}
-          <div className={collapsedClass}>
-            <StartForm
-              collapseForm={collapseForm}
-              openForm={openForm}
-              defaultForm={defaultOpenForm}
-            />
-            <AttractionsForm
-              formObject={attractions}
-              collapseForm={collapseForm}
-              openForm={openForm}
-              defaultForm={defaultOpenForm}
-            />
-            <AccommodationsForm
-              formObject={accommodations}
-              collapseForm={collapseForm}
-              openForm={openForm}
-              defaultForm={defaultOpenForm}
-            />
-            <FoodForm
-              formObject={food}
-              collapseForm={collapseForm}
-              openForm={openForm}
-              defaultForm={defaultOpenForm}
-            />
-            <DrinksForm
-              formObject={drinks}
-              collapseForm={collapseForm}
-              openForm={openForm}
-              defaultForm={defaultOpenForm}
-            />
-            <ServicesForm
-              formObject={services}
-              collapseForm={collapseForm}
-              openForm={openForm}
-              defaultForm={defaultOpenForm}
-            />
-            <SubmitTripForm
-              collapseForm={collapseForm}
-              openForm={openForm}
-              defaultForm={defaultOpenForm}
-            />
-          </div>
-        <button
-          className="collapse_button"
-          onClick={e => {
-            collapseFormContainer(!collapsed);
-          }}
-        >
-          {"||"}
-        </button>
+  return (
+    <div className="wrapper-div">
+      <div className={collapsedForm}>
+        <div className={collapsedClass}>
+          <StartForm
+            collapseForm={collapseForm}
+            openForm={openForm}
+            defaultForm={defaultOpenForm}
+          />
+          <AttractionsForm
+            formObject={attractions}
+            collapseForm={collapseForm}
+            openForm={openForm}
+            defaultForm={defaultOpenForm}
+          />
+          <AccommodationsForm
+            formObject={accommodations}
+            collapseForm={collapseForm}
+            openForm={openForm}
+            defaultForm={defaultOpenForm}
+          />
+          <FoodForm
+            formObject={food}
+            collapseForm={collapseForm}
+            openForm={openForm}
+            defaultForm={defaultOpenForm}
+          />
+          <DrinksForm
+            formObject={drinks}
+            collapseForm={collapseForm}
+            openForm={openForm}
+            defaultForm={defaultOpenForm}
+          />
+          <ServicesForm
+            formObject={services}
+            collapseForm={collapseForm}
+            openForm={openForm}
+            defaultForm={defaultOpenForm}
+          />
+          <SubmitTripForm
+            collapseForm={collapseForm}
+            openForm={openForm}
+            defaultForm={defaultOpenForm}
+          />
         </div>
+      <button
+        className="collapse_button"
+        onClick={e => {
+          collapseFormContainer(!collapsed);
+        }}
+      >
+        {"||"}
+      </button>
       </div>
-    );
-  }
-// };
+    </div>
+  );
+}
 
 export default FormsContainer;
